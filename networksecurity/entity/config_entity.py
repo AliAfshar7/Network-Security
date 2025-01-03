914390d4793778d2a3c3ccf7518d3b0894c0339a
AliAfshar7 <ali_a_78@yahoo.com>
2025-01-03 12:50:58 +0000

    data ingestion component

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -19,13 +19,13 @@ class DataIngestionConfig:
             training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME
         )
         self.feature_store_file_path: str = os.path.join(
-            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
         )
         self.training_file_path: str = os.path.join(
-            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
         )
         self.testing_file_path: str = os.path.join(
-            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
         )
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
