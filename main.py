6b86f15075af0c285ce5ac4051c56745b6630317
AliAfshar7 <ali_a_78@yahoo.com>
2025-01-10 23:46:20 +0000

    add some comments

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import sys
 
 if __name__ == "__main__":
     try:
+        # Data Ingestion part
         trainingpipelineconfig = TrainingPipelineConfig()
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
@@ -17,23 +18,25 @@ if __name__ == "__main__":
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data initiation completed")
         print(dataingestionartifact)
+        # Data Validation part
         data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Initiate the data validation")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("data validation completed")
         print(data_validation_artifact)
+        # Data Transformation part
         data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
         logging.info("data Transformation started")
         data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+        # Model Training part
         logging.info("Model Training Started")
         model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact = model_trainer.initiate_model_trainer()
-
         logging.info("Model Training Completed")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
